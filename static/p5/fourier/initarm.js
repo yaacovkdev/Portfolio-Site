5a1ff5b4485765e7d4cb8e59ca531f599c56f9f9
jacobkochatkov <jacob.kochatkov@gmail.com>
2024-11-03 01:38:37 -0700

    sandboxed fourier series graphics project with no document, window variables access

--- static/p5/fourier/initarm.js
+++ static/p5/fourier/initarm.js
@@ -1,4 +1,3 @@
-let code_field = "";
 let mainArm;
 
 let initial_code = `/*In this field you can program in javascript anything you like and set mainArm\n vectors with any type of algorithms*/\n
@@ -8,10 +7,32 @@ let initial_code = `/*In this field you can program in javascript anything you l
 //starting at PI/2\nmainArm.pushA(100,2,Math.PI/2);\n
 mainArm.push(10,30);`;
 document.getElementById("codefield").value = initial_code;
-code_field = document.getElementById("codefield").value;
+let code_field = initial_code;
+
+const sandboxFunction = (parsedJS) => {
+    return new Function(`
+    "use strict";
+    const window = undefined;
+    const document = undefined;
+    const alert = undefined;
+    const ActiveXObject = undefined;
+    const XMLHttpRequest = undefined;
+    const setTimeout = undefined;
+    const setInterval = undefined;
+    const clearTimeout = undefined;
+    const clearInterval = undefined;
+    const Function = undefined;
+
+    // Execute the user code in a scope where \`window\` and \`document\` are nullified
+    (function() {
+        ${parsedJS};
+    })();
+    
+`);
+}
 
 //change for now. Uncertain if it protects too much better.
-Function(`"use strict"; ${code_field}`)();
+sandboxFunction(code_field)();
 
 document.getElementById("buttonfield").onclick = function () {
   code_field = document.getElementById("codefield").value;
