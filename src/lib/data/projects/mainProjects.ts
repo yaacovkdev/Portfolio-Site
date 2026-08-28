51791a7440c5649171386dc27204e58233eaba2b
yaacovkdev <yaacov.kochatkov@gmail.com>
2026-08-28 15:03:13 -0700

    added livests project

--- src/lib/data/projects/mainProjects.ts
+++ src/lib/data/projects/mainProjects.ts
@@ -1,5 +1,5 @@
 import description_transcribesermons from "$lib/data/projects/descriptions/description_transcribesermons.html?raw";
-
+import description_livespeechtospeech from "$lib/data/projects/descriptions/description_livespeechtospeech.html?raw";
 export const MainProjectsInfo: { [key: string]: {
         name: string,
         desc: string,
@@ -13,5 +13,10 @@ export const MainProjectsInfo: { [key: string]: {
       name: "Sermons Transcribe",
       desc: description_transcribesermons,
       livelink: "https://sermons.yaacovk.dev",
+    },
+    livespeechtospeech: {
+        name: "Live Speech-To-Speech Translator Prototype",
+        desc: description_livespeechtospeech,
+        sourcelink: "https://github.com/SlavicEvangelicalBaptistChurch/LiveSTSPrototype"
     }
 };
